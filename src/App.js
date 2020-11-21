1bf45068c509e74ccc4f6b68032c40d1baf5f0e8
Rui Leite <leite@utfpr.edu.br>
2020-11-21 20:56:49 -0300

    Instala o `react-router-dom` e faz a configuração básica das rotas.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">
+          Início
+        </Link>
+        &nbsp;
+        <Link to="/nova-tarefa">
+          Nova tarefa
+        </Link>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            To-Do List
+          </Route>
+          <Route path="/nova-tarefa">
+            Nova tarefa
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
